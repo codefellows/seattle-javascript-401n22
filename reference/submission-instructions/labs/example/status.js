c5009f011fb5ffe12323082b6dffd5dddd3daaeb
David Souther <davidsouther+github@gmail.com>
2022-11-16 14:08:13 -0800

    First commit!
    Clean with only configs.
    
    Clean with only configs.

---
+++ reference/submission-instructions/labs/example/status.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const status = function (request) {
+  return {
+    status: 'running',
+    port: parseInt(process.env.PORT),
+    domain: request.hostname,
+  };
+};
+
+module.exports = status;
