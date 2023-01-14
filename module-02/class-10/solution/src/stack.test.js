358e654f1eec8d64bd9ae6feb2bf284840d07305
David Souther <davidsouther+github@gmail.com>
2023-01-14 09:02:05 -0800

    Lab 10 solution / lab 11 starter

---
+++ module-02/class-10/solution/src/stack.test.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const Stack = require("./stack");
+
+describe("Stack", () => {
+  test("sucessfully pops", () => {
+    let stack = new Stack();
+    stack.push("1");
+    stack.push("2");
+    stack.push("3");
+    let value = stack.pop();
+
+    expect(value).toEqual("3");
+    expect(stack.peek()).toEqual("2");
+  });
+
+  test("successfully peeks into a stack", () => {
+    let stack = new Stack();
+
+    stack.push("1");
+    stack.push("2");
+    stack.push("3");
+    let value = stack.peek();
+    expect(value).toEqual("3");
+  });
+
+  test("Should successfully empty a stack after multiple pops", () => {
+    let stack = new Stack();
+
+    stack.push("1");
+    stack.push("2");
+    stack.push("3");
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    expect(stack.peek()).toEqual(null);
+  });
+
+  test("successfully instantiates an empty stack", () => {
+    let stack = new Stack();
+    expect(stack.peek()).toEqual(null);
+  });
+
+  test("Calling pop or peek on empty stack raises exception", () => {
+    let stack = new Stack();
+    expect(() => {
+      stack.pop();
+    }).toThrow("Stack is Empty");
+  });
+});
