eec8e33c37b5995e9d0b64bbe819e3a96e9eafa5
David Souther <davidsouther+github@gmail.com>
2023-03-07 20:26:30 -0800

    lamest quickest dirtiest sneaky snacker ui

--- module-05/class-28/demo/src/App.js
+++ module-05/class-28/demo/src/App.js
@@ -1,22 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+
+export const KIDS_ROOM = "Melis' Play Cloud";
+export const HALLWAY = "Main Hallway";
+export const BATHROOM = "Diego's Delight";
+export const PARENTS_ROOM = "Parent's Chambers";
+export const GARAGE = "Boxes and Barels";
+export const LIVING_ROOM = "Dad's Dungeon";
+export const KITCHEN = "COOKIE HEAVEN";
+
+export const MAP = {
+  [KIDS_ROOM]: [HALLWAY, BATHROOM],
+  [BATHROOM]: [HALLWAY, KIDS_ROOM, PARENTS_ROOM],
+  [PARENTS_ROOM]: [HALLWAY, BATHROOM],
+  [HALLWAY]: [PARENTS_ROOM, BATHROOM, KIDS_ROOM, GARAGE, LIVING_ROOM, KITCHEN],
+  [GARAGE]: [HALLWAY],
+  [LIVING_ROOM]: [HALLWAY, KITCHEN],
+  [KITCHEN]: [LIVING_ROOM, HALLWAY],
+};
+
+export const OBJECTS = {
+  [KIDS_ROOM]: ["Backpack"],
+  [BATHROOM]: ["Stepstool"],
+  [PARENTS_ROOM]: ["Slippers"],
+  [HALLWAY]: ["Umbrella"],
+  [GARAGE]: ["Baseball Mit"],
+  [LIVING_ROOM]: ["Remote"],
+  [KITCHEN]: ["Cookie Jar"],
+};
+
+// COMMON: Object approach
+// ACTION: {action: "string action", payload: {detail: "action"}}
+
+export const MOVE = "move";
+export const PICKUP = "pickup";
+
+// SIMPLE: Tuple approach
+// ACTION: ["string action", "details of the action"]
+export function handleGame(state, action) {
+  if (action[0] === MOVE) {
+    const nextRoom = action[1];
+    if (MAP[state.room].includes(nextRoom)) {
+      state.room = nextRoom;
+    }
+  }
+
+  if (action[0] === PICKUP) {
+    const object = action[1];
+    if (!state.inventory.includes(object)) {
+      state.inventory = [...state.inventory, object];
+    }
+  }
+
+  return { ...state };
+}
+
+export const startState = {
+  room: KIDS_ROOM,
+  inventory: [],
+};
 
 function App() {
+  const [state, dispatch] = useReducer(handleGame, startState);
+
+  const room = state.room;
+  const inventory = state.inventory;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Melis is in: {room}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Inventory:{" "}
+          <ul>
+            {inventory.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {MAP[room].map((nextRoom) => (
+            <button key={nextRoom} onClick={() => dispatch([MOVE, nextRoom])}>
+              {nextRoom}
+            </button>
+          ))}
+        </p>
+        <p>
+          {OBJECTS[room]
+            .filter((item) => !inventory.includes(item))
+            .map((item) => (
+              <button key={item} onClick={() => dispatch([PICKUP, item])}>
+                {item}
+              </button>
+            ))}
+        </p>
       </header>
     </div>
   );
--- module-05/class-28/demo/src/App.test.js
+++ module-05/class-28/demo/src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App, {
+  MAP,
+  MOVE,
+  OBJECTS,
+  PICKUP,
+  handleGame,
+  startState,
+  BATHROOM,
+  KITCHEN,
+  GARAGE,
+} from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Can move", () => {
+  // Arrange
+  const initial = startState;
+
+  // Act
+  const state = handleGame(initial, [MOVE, BATHROOM]);
+
+  // Assert
+  expect(state.room).toBe(BATHROOM);
+});
+
+test("Can pick up", () => {
+  const initial = startState;
+
+  const state = handleGame(initial, [PICKUP, "Brush"]);
+
+  expect(state.inventory).toEqual(["Brush"]);
+});
+
+test("Cannot teleport", () => {
+  const initial = startState;
+
+  const state = handleGame(initial, [MOVE, KITCHEN]);
+
+  expect(state.room).not.toBe(KITCHEN);
+});
+
+test("Starts in the garage", () => {
+  const initial = { room: GARAGE, inventory: ["Backpack"] };
 });
