5123b931792f953ea321fadafe5b964df06eb374
John Cokos <highlander.44@gmail.com>
2023-03-28 20:37:31 -0700

    Adds Class 34 Demo and Drawings

--- module-06/class-34/demo/src/App.js
+++ module-06/class-34/demo/src/App.js
@@ -1,26 +1,75 @@
+import React, { useState, useEffect } from 'react';
 import Auth from './auth/Auth.jsx';
 import Login from './auth/Login.jsx';
+import axios from 'axios';
 import './app.css'
 
 import LoginContext from './auth/context.jsx';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+
+  const getTodoItems = async function() {
+    let url = `${process.env.REACT_APP_API}/api/v1/todo`
+    const response = await axios.get(url);
+    setItems( response.data.results)
+  }
+
+  const handleAddItem = async (e) => {
+    e.preventDefault();
+    let todoItem = {
+      text: text,
+      difficulty: 3,
+      assignee: "John",
+      complete: false,
+    }
+    const url = `${process.env.REACT_APP_API}/api/v1/todo`;
+
+    try {
+    const response = await axios.post(url, todoItem)
+    setItems( [...items, response.data]);
+    } catch (e) {
+      console.error( e.message)
+    }
+  }
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
+  useEffect( () => {
+    getTodoItems();
+  }, [])
+
   return (
     <LoginContext>
       <header>
         <Login />
       </header>
 
+
+
       <section>
         <div>All Users can see this</div>
 
         <Auth>
-          <div>If you are logged in, you can see this</div>
+          <ul>
+            {
+              items.map(item =>
+                <li key={item._Id}>{item.text}</li>
+              )
+            }
+          </ul>
         </Auth>
 
         <Auth capability="create">
-          <div>If you are logged in and have "create" permissions, you can see this</div>
+            <form onSubmit={handleAddItem}>
+              <input name="text" placeholder="Add Item" onChange={handleChange} />
+            </form>
         </Auth>
+
 
         <Auth capability="update">
           <div>If you are logged in and have "update" permissions, you can see this</div>
--- module-06/class-34/demo/src/auth/context.jsx
+++ module-06/class-34/demo/src/auth/context.jsx
@@ -1,29 +1,7 @@
 import React from 'react';
 import jwt_decode from 'jwt-decode';
 import jwtDecode from 'jwt-decode';
-
-const testUsers = {
-  Administrator: {
-    password: 'admin',
-    name: 'Administrator',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW5pc3RyYXRvciIsInJvbGUiOiJhZG1pbiIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJywncmVhZCcsJ3VwZGF0ZScsJ2RlbGV0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.pAZXAlTmC8fPELk2xHEaP1mUhR8egg9TH5rCyqZhZkQ'
-  },
-  Editor: {
-    password: 'editor',
-    name: 'Editor',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRWRpdG9yIiwicm9sZSI6ImVkaXRvciIsImNhcGFiaWxpdGllcyI6IlsncmVhZCcsJ3VwZGF0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.3aDn3e2pf_J_1rZig8wj9RiT47Ae2Lw-AM-Nw4Tmy_s'
-  },
-  Writer: {
-    password: 'writer',
-    name: 'Writer',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVyIiwicm9sZSI6IndyaXRlciIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.dmKh8m18mgQCCJp2xoh73HSOWprdwID32hZsXogLZ68'
-  },
-  User: {
-    password: 'user',
-    name: 'User',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVXNlciIsInJvbGUiOiJ1c2VyIiwiY2FwYWJpbGl0aWVzIjoiWydyZWFkJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.WXYvIKLdPz_Mm0XDYSOJo298ftuBqqjTzbRvCpxa9Go'
-  },
-};
+import axios from 'axios';
 
 export const LoginContext = React.createContext();
 
@@ -41,15 +19,23 @@ class LoginProvider extends React.Component {
     };
   }
 
-  login = ( username, password ) => {
+  login = async ( username, password ) => {
 
-    let { loggedIn, user, token } = this.state;
+    let url = process.env.REACT_APP_API;
 
-    let validUser = testUsers[username];
+    const axiosRequest = {
+      url: `${url}/signin`,
+      method: 'post',
+      auth: {
+        username, password
+      }
+    }
+    let response = await axios(axiosRequest)
+    const {token} = response.data;
 
-    if( validUser && validUser.password === password ) {
+    if(token) {
        try {
-         this.validateToken(validUser.token);
+         this.validateToken(token);
        } catch(e) {
          this.setLoginState( false, null, {}, e.message );
        }
