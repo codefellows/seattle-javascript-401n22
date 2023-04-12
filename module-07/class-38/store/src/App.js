cb556a8b2e185a309e6d3b05c7d3560f53a06b88
David Souther <davidsouther+github@gmail.com>
2023-04-11 19:04:03 -0700

    Load products thunk & effect

--- module-07/class-38/store/src/App.js
+++ module-07/class-38/store/src/App.js
@@ -1,7 +1,15 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { loadProducts } from "./products";
 
 function App() {
   const { products } = useSelector(({ products }) => products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadProducts());
+  }, []);
 
   return <div>Products: {products.length}</div>;
 }
--- module-07/class-38/store/src/products.js
+++ module-07/class-38/store/src/products.js
@@ -1,4 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const loadProducts = createAsyncThunk("products/load", async () => {
+  const response = await fetch(process.env.REACT_APP_API + "/products");
+  const json = await response.json();
+
+  return json.results;
+});
 
 const productsSlice = createSlice({
   name: "products",
@@ -6,6 +13,11 @@ const productsSlice = createSlice({
     products: [],
   },
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loadProducts.fulfilled, (state, { payload }) => {
+      state.products = payload;
+    });
+  },
 });
 
 export default productsSlice;
