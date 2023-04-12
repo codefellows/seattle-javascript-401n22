d20c2c9f6de71b6815aa372b9069e5b292a8b687
David Souther <davidsouther+github@gmail.com>
2023-04-11 18:44:24 -0700

    Starter app

---
+++ module-07/class-38/store/src/products.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState: {
+    products: [],
+  },
+  reducers: {},
+});
+
+export default productsSlice;
