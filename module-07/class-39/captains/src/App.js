0e9026752e2df74ab7d675c74b0e535738663c65
David Souther <davidsouther+github@gmail.com>
2023-04-15 09:32:02 -0700

    Demo starter code

---
+++ module-07/class-39/captains/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="fluid">
+        <nav>
+          <h1 style={{ "--color-h1": "var(--color-inverse)" }}>Captains</h1>
+        </nav>
+      </header>
+      <main></main>
+      <footer>&copy; 2023 David Souther & Code Fellows (js 401 n22)</footer>
+    </>
+  );
+}
+
+export default App;
