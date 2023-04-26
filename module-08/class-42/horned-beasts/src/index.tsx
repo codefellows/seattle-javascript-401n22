043b5c433a56e86dbd8d7da84c2c881930e5e86c
David Souther <davidsouther+github@gmail.com>
2023-04-25 20:08:22 -0700

    Service workers

--- module-08/class-42/horned-beasts/src/index.tsx
+++ module-08/class-42/horned-beasts/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
@@ -22,3 +23,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
